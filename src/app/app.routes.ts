ce03b6f709f17b2c0817e0c9f7966f1badd54645
Roberto Fontanarosa <r.fontanarosa@gmx.de>
2025-01-04 22:27:39 +0100

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+
+
+export const routes: Routes = [
+  {
+    path: 'start',
+    loadComponent: () => import('./pages/start/start.component').then(m => m.StartComponent)
+  }
+];
