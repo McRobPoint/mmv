ba86743dc0007a8cfabb99f7557cb712353d7e32
Roberto Fontanarosa <r.fontanarosa@gmx.de>
2025-01-05 15:07:18 +0100

    finished footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import {NavBarComponent} from "./core/components/nav-bar/nav-bar.component";
+import {MmvFooterComponent} from "./core/components/mmv-footer/mmv-footer.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavBarComponent],
+  imports: [CommonModule, RouterOutlet, NavBarComponent, MmvFooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
---
+++ src/app/core/components/mmv-footer/mmv-footer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mmv-footer',
+  standalone: true,
+  imports: [],
+  templateUrl: './mmv-footer.component.html',
+  styleUrl: './mmv-footer.component.scss'
+})
+export class MmvFooterComponent {
+
+}
